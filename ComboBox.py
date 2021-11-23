05d91a858bc1c35746048ffb1434d50d1af08fc8
Moti levi <levimoti75@gmail.com>
2021-11-23 15:16:15 +0200

    update

--- ComboBox.py
+++ ComboBox.py
@@ -12,16 +12,26 @@ ttk.Label(window, text = "RGo Testing",
           font = ("Times New Roman", 15)).grid(row = 0, column = 1)
   
 # label
-ttk.Label(window, text = "Select the Site :",
+ttk.Label(window, text = "Site :",
           font = ("Times New Roman", 10)).grid(column = 0,
-          row = 5, padx = 10, pady = 25)
+          row = 1,  column = 0,padx = 10, pady = 25)
+
+
+# label
+ttk.Label(window, text = "Station :",
+          font = ("Times New Roman", 10)).grid(column = 0,
+          row = 2, column = 0,padx = 10, pady = 25)
   
 # Combobox creation
-n = tk.StringVar()
-monthchoosen = ttk.Combobox(window, width = 27, textvariable = n)
+site = tk.StringVar()
+sitechoosen = ttk.Combobox(window, width = 27, textvariable = site)
+
+# Combobox creation
+station = tk.StringVar()
+stationChoosen = ttk.Combobox(window, width = 27, textvariable = station)
   
 # Adding combobox drop down list
-monthchoosen['values'] = (' January', 
+sitechoosen['values'] = (' January', 
                           ' February',
                           ' March',
                           ' April',
@@ -33,7 +43,16 @@ monthchoosen['values'] = (' January',
                           ' October',
                           ' November',
                           ' December')
-  
-monthchoosen.grid(column = 1, row = 5)
-monthchoosen.current()
+stationChoosen['values'] = (' 1', 
+                          ' 2',
+                          ' 3')  
+
+
+sitechoosen.grid(column = 1, row = 2)
+sitechoosen.current()
+
+stationChoosen.grid(column = 2, row = 2)
+stationChoosen.current()
+
+
 window.mainloop()
