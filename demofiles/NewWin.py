b57ecc8e9af0e9921a1081dd922838e512b4db20
Moti levi <levimoti75@gmail.com>
2021-11-29 15:57:53 +0200

    commit

--- demofiles/NewWin.py
+++ demofiles/NewWin.py
@@ -22,3 +22,41 @@
 
 # root.mainloop()
 
+from tkinter import *
+
+class TestApp:
+    def __init__(self, master):
+        self.master = master
+        master.minsize(width=800, height=640)
+
+        self.button = Button(master, text='Next', command=self.firstWindow)
+        self.button.pack()
+
+    def firstWindow(self):
+        self.master.withdraw()
+        self.newfirstWindow = Toplevel(self.master)
+        self.newfirstWindow.minsize(width=800, height=640)
+
+        self.button = Button(self.newfirstWindow, text='Next', command=self.secondWindow)
+        self.button.pack()
+
+    def secondWindow(self):
+        self.newfirstWindow.destroy()
+        self.newsecondWindow = Toplevel(self.master)
+        self.newsecondWindow.minsize(width=800, height=640)
+
+        self.button = Button(self.newsecondWindow, text='Quit', command=self.master.quit())
+        self.button.pack()
+
+
+
+
+
+if __name__ == "__main__":
+    master = Tk()
+    App = TestApp(master)
+    master.mainloop()
+
+
+
+
